5a628034550befc45782a4db9dabf98295623622
gorkemdurgun <gorkemdurgunn@gmail.com>
2024-04-05 12:54:28 +0300

    Update toast component and add loading toast

--- components/toaster.tsx
+++ components/toaster.tsx
@@ -1,11 +1,27 @@
-import toast from "react-hot-toast";
+import toast, { ToastOptions } from "react-hot-toast";
+
+const defaultOptions:ToastOptions = {
+  duration: 5000,
+  position: "top-center"
+};
+
+const toastController = toast;
 
 const success = (message: string) => {
-  return toast.success(message);
+  return toast.success(message, defaultOptions);
 };
 
 const error = (message: string) => {
-  return toast.error(message);
+  return toast.error(message, defaultOptions);
 };
 
-export { success as successToast, error as errorToast };
+const loading = () => {
+  return toast.loading("Please wait...", defaultOptions);
+};
+
+export {
+  toastController,
+  success as successToast,
+  error as errorToast,
+  loading as loadingToast
+};
